270e84bb22bb2e4d583f842bb06b8d4d695660fe
Yagna yamala <146903789+yagna911@users.noreply.github.com>
2023-10-24 14:46:49 +0530

    custom-script.js

---
+++ custom-script.js
@@ -0,0 +1,17 @@
+const ctaButton = document.querySelector('.cta-button');
+
+
+
+ctaButton.addEventListener('mouseover', () => {
+
+    ctaButton.style.backgroundColor = '#ff0000';
+
+});
+
+
+
+ctaButton.addEventListener('mouseout', () => {
+
+    ctaButton.style.backgroundColor = '#e50914';
+
+});
